bb4810fb2754d3a2ed044e0447c4d16111368566
Edward Wang <87530153+EdJishWang@users.noreply.github.com>
2021-07-30 11:53:04 -0400

    Update __main__.py

---
+++ fifeforspark/__main__.py
@@ -0,0 +1,20 @@
+import processors, utils
+
+def parse_config() -> dict:
+    """Parse configuration parameters specified in the command line."""
+    parser = utils.FIFEArgParser()
+    args = parser.parse_args()
+    config = {}
+    config.update({k: v for k, v in vars(args).items() if v is not None})
+    return config
+
+def main():
+    config = parse_config()
+    spark_df = utils.create_example_data2(n_persons = 100, n_periods = 12)
+    data_processor = processors.PanelDataProcessor(config, spark_df)
+    data_processor.build_processed_data()
+    data_processor.spark_df.show()
+    
+
+if __name__ == '__main__':
+    main()
