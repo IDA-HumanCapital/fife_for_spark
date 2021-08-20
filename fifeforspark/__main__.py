e101b20f6e2df2e11e2c940245bf1be93729437f
ajain-ida <69818647+ajain-ida@users.noreply.github.com>
2021-08-20 11:21:22 -0400

    Added docstrings to main

--- fifeforspark/__main__.py
+++ fifeforspark/__main__.py
@@ -1,16 +1,28 @@
 import processors, utils
 
+
 def parse_config() -> dict:
-    """Parse configuration parameters specified in the command line."""
+    """
+    Parse configuration parameters specified in the command line.
+    Returns:
+        Configuration dictionary generated from the command line input
+    """
+
     parser = utils.FIFEArgParser()
     args = parser.parse_args()
     config = {}
     config.update({k: v for k, v in vars(args).items() if v is not None})
     return config
 
+
 def main():
+    """
+    Executable code of FIFE when run from the command line
+    Returns:
+
+    """
     config = parse_config()
-    spark_df = utils.create_example_data2(n_persons = 100, n_periods = 12)
+    spark_df = utils.create_example_data2(n_persons=100, n_periods=12)
     data_processor = processors.PanelDataProcessor(config, spark_df)
     data_processor.build_processed_data()
     data_processor.spark_df.show()
