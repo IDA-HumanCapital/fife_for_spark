8b1d43758ab6b729b00a115a0701fd65eb48e325
Edward Wang <87530153+EdJishWang@users.noreply.github.com>
2021-08-20 12:00:52 -0400

    Update __main__.py

--- fifeforspark/__main__.py
+++ fifeforspark/__main__.py
@@ -1,4 +1,4 @@
-import processors, utils
+import processors, utils, lgb_modelers
 
 def parse_config() -> dict:
     """Parse configuration parameters specified in the command line."""
@@ -14,6 +14,11 @@ def main():
     data_processor = processors.PanelDataProcessor(config, spark_df, shuffle_parts = 50)
     data_processor.build_processed_data()
     data_processor.data.show()
+    
+    modeler_class = lgb_modelers.LGBSurvivalModeler    
+    modeler = modeler_class(config=config, data=data_processor.data)
+    modeler.build_model(n_intervals=modeler.n_intervals)
+
 
 if __name__ == '__main__':
     main()
