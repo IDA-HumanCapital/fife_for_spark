5fef2f79ca7a5662699ad8292a0205d9843ad9ce
Edward Wang <87530153+EdJishWang@users.noreply.github.com>
2021-08-23 09:43:52 -0400

    Fix predict_col error

--- fifeforspark/base_modelers.py
+++ fifeforspark/base_modelers.py
@@ -135,7 +135,7 @@ class Modeler(ABC):
         data: Union[None, pyspark.sql.DataFrame] = None,
         duration_col: str = "_duration",
         event_col: str = "_event_observed",
-        predict_col: str = "_predict_obs",
+        predict_col: str = "_predict_col",
         test_col: str = "_test",
         validation_col: str = "_validation",
         period_col: str = "_period",
