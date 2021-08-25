41514f2e04ff4bc9396a5f0ff6ae836f2dc600bf
ajain-ida <69818647+ajain-ida@users.noreply.github.com>
2021-08-25 13:06:26 -0400

    rebuilt whl file

--- update_whl_file.bat
+++ update_whl_file.bat
@@ -3,9 +3,6 @@
 call C:/Users/%username%/Miniconda3/Scripts/activate.bat
 call C:/Users/%username%/AppData/Local/Continuum/anaconda3/Scripts/activate.bat
 
-call C:/Users/%username%/Miniconda3/Scripts/activate.bat fifeforspark
-call C:/Users/%username%/AppData/Local/Continuum/anaconda3/Scripts/activate.bat fifeforspark
-
 python setup.py sdist bdist_wheel
 
 pause
