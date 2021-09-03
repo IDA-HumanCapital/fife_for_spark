53d1aad9866186723f9f2dc515c1bcef712362cd
Edward Wang <87530153+EdJishWang@users.noreply.github.com>
2021-09-03 09:20:18 -0400

    Remove redundant Koalas transformation

--- build/lib/fifeforspark/processors.py
+++ build/lib/fifeforspark/processors.py
@@ -216,11 +216,6 @@ class PanelDataProcessor(DataProcessor):
             Spark DataFrame with reserved columns added
         """
 
-        ks_df = ks.DataFrame(self.data)
-        ks_df['_period'] = ks_df[self.config['TIME_IDENTIFIER']].factorize(sort=True)[
-            0]
-        self.data = ks_df.to_spark()
-
         max_val = lit(self.data.agg({'_period': 'max'}).first()[0])
         self.data = self.data.withColumn(
             '_predict_obs', self.data['_period'] == max_val)
